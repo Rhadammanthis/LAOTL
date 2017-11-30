6c73932f36b5691e1757d811cf85c5c50eaaaf22
Hugo Medina <rhadammanthis@gmail.com>
2017-11-30 00:48:46 +0000

    Refactored color constants

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -5,8 +5,9 @@ import {
     TouchableOpacity, Image, Modal, TouchableNativeFeedback
 } from 'react-native';
 import Transparency from './common/Transparency'
+import Constants from './common/Constants'
 import { connect } from 'react-redux';
-import { selectEpisode, toggleNavbarAlpha } from '../actions'
+import { selectEpisode, toggleNavbarFade } from '../actions'
 
 class Episode extends Component {
 
@@ -24,36 +25,29 @@ class Episode extends Component {
 
         return (
             <View style={{ flex: 1 }}>
-                <ScrollView style={{ backgroundColor: '#171612' }}
+                <ScrollView style={{ backgroundColor: Constants.COLOR.BACKGROUND }}
                     onScroll={(event) => {
-                        console.log('Scroll', event.nativeEvent.contentOffset.y);
-                        this.props.toggleNavbarAlpha(event.nativeEvent.contentOffset.y)
+                        this.props.toggleNavbarFade(event.nativeEvent.contentOffset.y)
                     }}>
                     <Image style={{ flex: 1, height: 250 }} source={{ uri: this.props.selectedEpisode.image }} />
                     <Transparency size={35} />
                     <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <Text style={{ flex: 8, color: '#F37752', fontWeight: 'bold', fontSize: 35, marginTop: -25, marginHorizontal: 10,
-                            opacity: (1 - this.props.alpha) }}>
+                        <Text style={[styles.title, { opacity: (1 - this.props.fade) }]}>
                             {this.props.selectedEpisode.title}
                         </Text>
                         <View style={{ flex: 2, flexDirection: 'row' }}>
-                            <Image style={{
-                                marginTop: -5,
-                                height: 25,
-                                width: 25,
-                                tintColor: '#F6502C',
-                            }} source={require('../images/clock.png')} />
-                            <Text style={{ color: '#F37752', marginHorizontal: 10 }}>
+                            <Image style={styles.icon} source={require('../images/clock.png')} />
+                            <Text style={{ color: Constants.COLOR.MUTE_ORANGE, marginHorizontal: 10 }}>
                                 {this.props.selectedEpisode.duration}
                             </Text>
                         </View>
                     </View>
-                    <Text style={{ flex: 2, color: 'white', marginHorizontal: 10, marginTop: 10 }}>
+                    <Text style={styles.description}>
                         {this.props.selectedEpisode.description}
                     </Text>
                     <View style={{ height: 1000 }} />
                 </ScrollView>
-                <View style={[styles.navBar, { opacity: this.props.alpha, }]}>
+                <View style={[styles.navBar, { opacity: this.props.fade, }]}>
                     <Text style={styles.navBarTitle}>
                         {this.props.selectedEpisode.title}
                     </Text>
@@ -76,7 +70,7 @@ const styles = StyleSheet.create({
         bottom: 0,
         left: 0,
         right: 0,
-        backgroundColor: '#211f19',
+        backgroundColor: Constants.COLOR.TOOLBAR,
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
@@ -84,7 +78,7 @@ const styles = StyleSheet.create({
     navBarTitle: {
         flex: 1,
         fontSize: 18,
-        color: '#F37752',
+        color: Constants.COLOR.MUTE_ORANGE,
         fontWeight: 'bold',
         marginLeft: 35
     },
@@ -98,23 +92,43 @@ const styles = StyleSheet.create({
     backButtonImage: {
         height: 25,
         width: 25,
-        tintColor: '#F6502C',
+        tintColor: Constants.COLOR.BRIGHT_ORANGE,
         marginLeft: 5,
         position: 'absolute',
         top: 13,
         bottom: 0,
         left: 0,
         right: 0,
+    },
+    description: {
+        flex: 2,
+        color: 'white',
+        marginHorizontal: 10,
+        marginTop: 10
+    },
+    icon: {
+        marginTop: -5,
+        height: 25,
+        width: 25,
+        tintColor: Constants.COLOR.BRIGHT_ORANGE,
+    },
+    title: {
+        flex: 8,
+        color: Constants.COLOR.MUTE_ORANGE,
+        fontWeight: 'bold',
+        fontSize: 35,
+        marginTop: -25,
+        marginHorizontal: 10
     }
 });
 
 const mapStateToProps = ({ data }) => {
 
-    const { selectedEpisode, alpha } = data;
+    const { selectedEpisode, fade } = data;
 
     return {
-        selectedEpisode, alpha
+        selectedEpisode, fade
     };
 };
 
-export default connect(mapStateToProps, { toggleNavbarAlpha })(Episode)
+export default connect(mapStateToProps, { toggleNavbarFade })(Episode)
---
+++ src/components/common/Constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    COLOR: {
+        BRIGHT_ORANGE: "#F6502C",
+        MUTE_ORANGE: "#F37752",
+        BACKGROUND: "#171612",
+        TOOLBAR: "#211f19"
+    }
+}
