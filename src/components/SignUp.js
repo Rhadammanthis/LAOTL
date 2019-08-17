3f1ae400770459a423c651256b8afaa1daa55351
Hugo Medina <rhadammanthis@gmail.com>
2019-08-17 18:20:32 +0000

    Updated the rendering logic of both forms

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -49,6 +49,12 @@ class SignUp extends Component {
                     buttonText="Log in"
                     fields={[
                         {
+                            label: "Name",
+                            icon: "md-person",
+                            required: true,
+                            type: Types.Text
+                        },
+                        {
                             label: "Email",
                             icon: "md-mail",
                             required: true,
@@ -104,12 +110,16 @@ class SignUp extends Component {
 
     render() {
 
+        var logInForm = this.state.loginIn ? this._renderLoginForm() : null
+        var signUpForm = !this.state.loginIn ? this._renderSignUpForm() : null
+
         return (
             <View style={{ flex: 1, backgroundColor: COLORS.BACKGROUND, }}>
                 <View style={{ flex: 2 }}>
 
                 </View>
-                {this.state.loginIn ? this._renderLoginForm() : this._renderSignUpForm()}
+                {logInForm}
+                {signUpForm}
                 <View style={{ flexDirection: 'row', justifyContent: "center", alignItems: "center", marginVertical: 20 }}>
                     <View style={{ width: 100, height: 2, backgroundColor: "grey" }}></View>
                     <Text style={{ color: "grey" }}> OR </Text>
@@ -119,8 +129,9 @@ class SignUp extends Component {
                     <Text style={{ color: "grey" }}>{this.state.loginIn ? "Don't have an account yet?" : "Already have an account?"} </Text>
                     <TouchableOpacity onPress={() => { 
                         LayoutAnimation.configureNext({
-                            duration: 400,
+                            duration: 300,
                             create: {
+                                delay: 80,
                                 type: LayoutAnimation.Types.spring,
                                 property: LayoutAnimation.Properties.scaleXY,
                                 springDamping: 0.7,
@@ -131,12 +142,13 @@ class SignUp extends Component {
                                 springDamping: 0.7,
                             },
                             delete: {
-                                type: LayoutAnimation.Types.spring,
-                                property: LayoutAnimation.Properties.scaleXY,
-                                springDamping: 0.7,
+                                duration: 100,
+                                type: LayoutAnimation.Types.linear,
+                                property: LayoutAnimation.Properties.opacity,
                             }
                         })
-                        this.setState({ loginIn: !this.state.loginIn })}}>
+                        this.setState({ loginIn: !this.state.loginIn })}
+                    }>
                         <Text style={{ color: COLORS.BRIGHT_ORANGE }}>{this.state.loginIn ? " Sign up" : " Log in"}</Text>
                     </TouchableOpacity>
                 </View>
